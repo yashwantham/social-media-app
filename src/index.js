fbec3321e82607c972b9960ac43299944dce4b23
Yashwanth <yashwanthyashu166@gmail.com>
2023-06-09 23:53:53 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
