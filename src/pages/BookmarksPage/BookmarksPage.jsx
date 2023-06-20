b394cd7bec4d7b6b5914e9e2cf1dc46386d447d2
Yashwanth <yashwanthyashu166@gmail.com>
2023-06-21 02:06:45 +0530

    refactor: change in bookmarks page

--- src/pages/BookmarksPage/BookmarksPage.jsx
+++ src/pages/BookmarksPage/BookmarksPage.jsx
@@ -1,18 +1,45 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TopNav } from "../../components/TopNav/TopNav";
 import "./BookmarksPage.css";
 import { DataContext } from "../../contexts/DataProvider";
 import { PostCard } from "../HomePage/HomePageComponents/PostCard/PostCard";
+import axios from "axios";
+import { ACTIONS } from "../../reducers/DataRedcuer";
 
 export function BookmarksPage() {
 
-    const {dataState} = useContext(DataContext);
+    const authToken = localStorage.getItem("userToken")
+
+    const {dispatchData, dataState} = useContext(DataContext);
+
+    const {SET_BOOKMARKS} = ACTIONS;
+
+    const [bm, setBm] = useState([]);
+
+    const getBookmarks = async () => {
+        try {
+            const response = await axios.get("/api/users/bookmark/", {headers: { authorization: authToken }})
+            console.log("getBookmarks", response)
+            if(response.status === 200) {
+                dispatchData({type: SET_BOOKMARKS, payload: response.data.bookmarks})
+                setBm(response.data.bookmarks)
+            }
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getBookmarks();
+    }, [])
 
     return (
         <>
         <div className="bookmarks-page-container">
             <TopNav pageName={"Bookmarks"}/>
-            {dataState.bookmarks.map((post) => <PostCard post={post}/>)}
+            {/* {dataState.bookmarks.map((post) => <PostCard post={post}/>)} */}
+            {bm.map((post) => <PostCard post={post}/>)}
         </div>
         </>
     )
