1662fad2c7f56d61efd95b39f587b06c69913e63
Yashwanth <yashwanthyashu166@gmail.com>
2023-06-21 00:42:18 +0530

    feat: built bookmarks page

--- src/pages/BookmarksPage/BookmarksPage.jsx
+++ src/pages/BookmarksPage/BookmarksPage.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import { TopNav } from "../../components/TopNav/TopNav";
 import "./BookmarksPage.css";
+import { DataContext } from "../../contexts/DataProvider";
+import { PostCard } from "../HomePage/HomePageComponents/PostCard/PostCard";
 
 export function BookmarksPage() {
+
+    const {dataState} = useContext(DataContext);
+
     return (
         <>
         <div className="bookmarks-page-container">
             <TopNav pageName={"Bookmarks"}/>
-            <h1>Bookmarks Page</h1>
+            {dataState.bookmarks.map((post) => <PostCard post={post}/>)}
         </div>
         </>
     )
